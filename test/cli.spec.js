91b2ca0cd0a0ed208580e2ba6b77bd0f4a6a25ae
Titouan Mathis <titouan@studiometa.fr>
2024-12-11 17:03:02 +0100

    Add tests

--- test/cli.spec.js
+++ test/cli.spec.js
@@ -1,7 +1,6 @@
-import { describe, it, expect, afterEach, spyOn } from 'bun:test';
+import { describe, it, expect, afterEach } from 'bun:test';
 import { existsSync, unlinkSync, readFileSync } from 'node:fs';
 import { execSync } from 'node:child_process';
-import { prettierFormatterGitLab } from '../src/index.js';
 
 const CODE_QUALITY_FILENAME = 'gl-prettier-codequality.json';
 
@@ -12,7 +11,7 @@ afterEach(() => {
 });
 
 describe('prettier-formatter-gitlab cli', () => {
-  it('should create a code quality report file', () => {
+  it('should create a code quality report file with prettier -l', () => {
     try {
       execSync(
         `PRETTIER_CODE_QUALITY_REPORT="${CODE_QUALITY_FILENAME}" ./bin/cli.js "prettier -l test/__stubs__/"`,
@@ -22,11 +21,11 @@ describe('prettier-formatter-gitlab cli', () => {
     }
 
     expect(existsSync(CODE_QUALITY_FILENAME)).toBe(true);
-    const content = readFileSync(CODE_QUALITY_FILENAME);
-    expect(content.toString()).toMatchSnapshot();
+    const content = JSON.parse(readFileSync(CODE_QUALITY_FILENAME).toString());
+    expect(content).toMatchSnapshot();
   });
 
-  it('should create a code quality report file', () => {
+  it('should create a code quality report file with prettier -c', () => {
     try {
       execSync(
         `PRETTIER_CODE_QUALITY_REPORT="${CODE_QUALITY_FILENAME}" ./bin/cli.js "prettier -c test/__stubs__/"`,
@@ -36,31 +35,7 @@ describe('prettier-formatter-gitlab cli', () => {
     }
 
     expect(existsSync(CODE_QUALITY_FILENAME)).toBe(true);
-    const content = readFileSync(CODE_QUALITY_FILENAME);
-    expect(content.toString()).toMatchSnapshot();
-  });
-
-  it('should work with the `-c` or `-l` flag', async () => {
-    const consoleSpy = spyOn(console, 'log');
-    consoleSpy.mockImplementation(() => null);
-    process.env.PRETTIER_CODE_QUALITY_REPORT = CODE_QUALITY_FILENAME;
-
-    // prettier -c path/to/folder/
-    const outputCheck = `Checking formatting...
-[warn] test/__stubs__/dirty-2.js
-[warn] test/__stubs__/dirty.js
-[warn] Code style issues found in 3 files. Run Prettier to fix.
-`;
-
-    // prettier -l path/to/folder/
-    const outputList = `test/__stubs__/dirty-2.js
-test/__stubs__/dirty.js
-`;
-
-    await prettierFormatterGitLab(outputCheck);
-    await prettierFormatterGitLab(outputList);
-    expect(consoleSpy).toHaveBeenCalledTimes(12);
-    expect(consoleSpy.mock.calls.slice(0, 6)).toEqual(consoleSpy.mock.calls.slice(6, 12));
-    consoleSpy.mockRestore();
+    const content = JSON.parse(readFileSync(CODE_QUALITY_FILENAME).toString());
+    expect(content).toMatchSnapshot();
   });
 });
